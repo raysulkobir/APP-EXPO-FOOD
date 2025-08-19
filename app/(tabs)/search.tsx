cfa23620fc5d4f27532689c9f647635af95fd964
Md.Raysul kobir <raysul@thals.org>
2025-08-20 00:02:12 +0600

    search page search secitn

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { COLORS } from '@/constants/colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
+import { TextInput, TouchableOpacity, View } from 'react-native';
+import { searchStyles } from "../../assets/styles/search.styles";
 
 const search = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [initialLoading, setInitialLoading] = useState(true);
   return (
-    <View>
-      <Text>search</Text>
+    <View style={searchStyles.container}>
+      <View style={searchStyles.searchSection}>
+        <View style={searchStyles.searchContainer}>
+            <Ionicons
+              name="search"
+              size={20}
+              color={COLORS.textLight}
+              style={searchStyles.searchIcon}
+            />
+
+            <TextInput
+              style={searchStyles.searchInput}
+              placeholder="Search recipes, ingredients..."
+              placeholderTextColor={COLORS.textLight}
+              value={searchQuery}
+              onChangeText={setSearchQuery}
+              returnKeyType='search'
+            />
+
+          <TouchableOpacity onPress={() => setSearchQuery("")} style={searchStyles.clearButton}>
+            <Ionicons name="close-circle" size={20} color={COLORS.textLight} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
